767d7f9d83a9b6ab161f4c4f81fa94403f6c33b4
Gautham Hari <124058781+GauthamHar1@users.noreply.github.com>
2026-07-31 20:11:57 -0400

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # we can do a modified binary search here
+        # so we can start at the very middle of the middle
+        # of the array so we can have a top down ptr and 
+        # a left right ptr
+
+        u,l = 0,0
+        d,r = len(matrix)-1,len(matrix[0])-1
+        while u<=d:
+            mid = u+((d-u)//2)
+            if target >= matrix[mid][l] and target<=matrix[mid][r]:
+                while l<=r:
+                    mid1 = l+((r-l)//2)
+                    if matrix[mid][mid1] == target:
+                        return True
+                    elif matrix[mid][mid1] < target:
+                        l = mid1+1
+                    else:
+                        r = mid1-1
+                return False
+            elif target > matrix[mid][r]:
+                u = mid+1
+            else:
+                d = mid-1
+        return False
+            
+
+        
